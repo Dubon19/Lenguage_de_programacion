6cc1f353e4f063f983fe0c5759cb894e4bb04fbb
Dubon19 <duboncliffor@gmail.com>
2024-02-03 09:56:15 -0600

    Ejemplo de uso de funciones

---
+++ session3.py
@@ -0,0 +1,7 @@
+import os
+os.system("cls || Clear")
+def get_full_name(name, last_name):
+    full_name = name + ' ' + last_name
+    return full_name
+
+print( f"hola {get_full_name('cliffor','Dubon')}")
